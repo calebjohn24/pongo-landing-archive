7c6256f666b232ba0af51c6da55ff678951290d9
czearing <czearing@outlook.com>
2022-03-22 21:22:18 -0700

    Adding workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <span style={{ color: 'red' }}>This is a test</span>;
+  return <span style={{ color: 'red' }}>This is a test asdadsasdad</span>;
 };
 
 export default Home;
