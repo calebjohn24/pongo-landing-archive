8e4d4471517f2036c83a0cee6d38f5bc26218162
czearing <czearing@outlook.com>
2022-03-22 21:20:07 -0700

    Updating files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div />;
+  return <span style={{ color: 'red' }}>This is a test</span>;
 };
 
 export default Home;
