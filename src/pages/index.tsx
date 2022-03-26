45597c70622eb745f7749e7c33b565bc94c07c01
czearing <czearing@outlook.com>
2022-03-25 17:20:58 -0700

    Adding component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,31 +2,30 @@ import * as React from 'react';
 import {
   Text,
   Container,
+  Card,
+  CardHeader,
+  CardRow,
   Display,
   Stack,
   Body,
   Header1,
+  Header2,
   LinkedinIcon,
   TwitterIcon,
+  ToggleButton,
   Button,
   SubHeadline,
-  createIcon,
+  InstagramIcon,
   LogoIcon,
   Divider,
   tokens,
   Header3,
+  CardMedia,
 } from '@pongo-ui/react-components';
 import { makeStyles } from '@griffel/react';
 import Image from 'next/image';
 import type { NextPage } from 'next';
 import { ContactForm } from '../components';
-
-const InstagramIcon = createIcon({
-  path: (
-    <path d="M682.533 204.8C419.092 204.8 204.8 419.29 204.8 682.8V1365.47C204.8 1628.91 419.29 1843.2 682.8 1843.2H1365.47C1628.91 1843.2 1843.2 1628.71 1843.2 1365.2V682.533C1843.2 419.092 1628.71 204.8 1365.2 204.8H682.533ZM1501.87 477.866C1539.55 477.866 1570.13 508.45 1570.13 546.133C1570.13 583.816 1539.55 614.4 1501.87 614.4C1464.18 614.4 1433.6 583.816 1433.6 546.133C1433.6 508.45 1464.18 477.866 1501.87 477.866ZM1024 614.4C1249.89 614.4 1433.6 798.105 1433.6 1024C1433.6 1249.89 1249.89 1433.6 1024 1433.6C798.105 1433.6 614.4 1249.89 614.4 1024C614.4 798.105 798.105 614.4 1024 614.4ZM1024 750.933C951.578 750.933 882.122 779.703 830.912 830.912C779.703 882.122 750.933 951.578 750.933 1024C750.933 1096.42 779.703 1165.88 830.912 1217.09C882.122 1268.3 951.578 1297.07 1024 1297.07C1096.42 1297.07 1165.88 1268.3 1217.09 1217.09C1268.3 1165.88 1297.07 1096.42 1297.07 1024C1297.07 951.578 1268.3 882.122 1217.09 830.912C1165.88 779.703 1096.42 750.933 1024 750.933V750.933Z" />
-  ),
-  displayName: 'InstagramIcon',
-});
 
 const useStyles = makeStyles({
   mainSection: {
@@ -42,6 +41,9 @@ const useStyles = makeStyles({
   iconStyles: {
     width: 'min(12vw, 128px)',
     height: 'min(12vw, 128px)',
+  },
+  emojiButtonStyles: {
+    fontSize: '30px',
   },
 });
 
@@ -104,14 +106,40 @@ const Home: NextPage = () => {
         </Stack>
         <Divider />
         <Stack appearance="relaxed" horizontalAlignment="center" verticalAlignment="center">
-          <Image width="400px" height="400px" src="image/survey-demo.png" />
+          <Card inline>
+            <CardHeader header={<Header2>Printed Tote Bag</Header2>} />
+            <Stack horizontalAlignment="center">
+              <Image src={'image/bag.png'} width="200px" height="200px" />
+            </Stack>
+            <Body>How happy are you with your purchases?</Body>
+            <Stack horizontalAlignment="center">
+              <Button className={styles.emojiButtonStyles} size="large" shape="circle">
+                😡
+              </Button>
+              <Button className={styles.emojiButtonStyles} size="large" shape="circle">
+                😐
+              </Button>
+              <Button className={styles.emojiButtonStyles} size="large" shape="circle">
+                😄
+              </Button>
+            </Stack>
+            <Body>What could have gone better?</Body>
+            <Stack>
+              <ToggleButton shape="circular">Results</ToggleButton>
+              <ToggleButton shape="circular">Packaging</ToggleButton>
+              <ToggleButton shape="circular">Price</ToggleButton>
+            </Stack>
+            <Button color="brand" appearance="primary">
+              Submit
+            </Button>
+          </Card>
           <Stack vertical>
             <Header1>An Interface Built for Conversions 📈</Header1>
             <Body className={styles.textMaxWidth} size={500}>
-              Our feedback interface is built from the ground up to be intuitive and easy for the customer to fill out.
+              Our feedback interface is built from the ground up to be intuitive for customers to fill out.
             </Body>
             <Body className={styles.textMaxWidth} size={500}>
-              Don't settle for clunky surveys your customer will close out of.
+              We strive to make this workflow as easy as possible.
             </Body>
           </Stack>
         </Stack>
